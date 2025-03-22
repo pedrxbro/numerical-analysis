d6b5a4b43dd6c7619f6c8a1739368cca31c81a93
pedrxbro <pedrohdpaula@edu.univali.br>
2025-03-22 10:22:02 -0300

    newton-raphson-method

--- newton-raphson-method/main.py
+++ newton-raphson-method/main.py
@@ -1,14 +1,10 @@
-quoficient = []
-power = []
-n = 0
-
 def inputData():
     global n
-    x0 = int(input("Digite o valor de x0: "))
+    x0 = float(input("Digite o valor de x0: "))
     n = int(input("Digite o número de termos: "))
 
     for i in range (n):
-        quoficient.append(int(input(f"Digite o quoficiente do termo {i + 1}: ")))
+        quoficient.append(float(input(f"Digite o quoficiente do termo {i + 1}: ")))
         power.append(n - 1 - i)
         
     toler = float(input("Digite o erro percentual: "))
@@ -22,31 +18,20 @@ def calculateF(x):
     return result
 
 def calculateDerivative(x):
-    derivateQuoficient = []
-    derivatePower = []
-
-    for i in range (len(quoficient)):
-        if(power[i] > 0):
-            derivateQuoficient.append(quoficient[i] * power[i]) # Multiplica o quoficiente pela potência
-            derivatePower.append(power[i] - 1) # Diminui a potência
-
     derivateValue = 0
-    for i in range (len(derivateQuoficient)):
-        derivateValue += derivateQuoficient[i] * (x ** derivatePower[i])
-    return derivateQuoficient, derivatePower
+    for i in range (len(quoficient)):
+        if power[i] > 0:
+            derivateValue += quoficient[i] * power[i] * (x ** (power[i] - 1))
+    return derivateValue
     
-def evaluatePolynomial(quoficient, power, x):
-    return sum (quoficient * ( x ** power) for quoficient, power in zip(quoficient, power))
 
 def newtonRaphsonMethod(x0, toler, iterMax):
     iter = 0
     erro = 100
 
-    derivateQuoficient, derivatePower = calculateDerivative(x0)
-
     while(erro > toler and iter < iterMax):
         fx0 = calculateF(x0)
-        fDerivateX0 = evaluatePolynomial(derivateQuoficient, derivatePower, x0)
+        fDerivateX0 = calculateDerivative(x0)
 
         if fDerivateX0 == 0:
             print("Derivada igual a zero. Não é possível continuar") 
@@ -56,14 +41,17 @@ def newtonRaphsonMethod(x0, toler, iterMax):
         erro = abs((xNext - x0) / xNext) * 100
         x0 = xNext
         iter += 1
-        print(f"Iteração: {iter} - Raíz: {x0} - Erro: {erro}")
+        print(f"Iteração: {iter} - Raíz: {x0:.4f} - Erro: {erro:.4f}%")
     return x0
 
+quoficient = []
+power = []
+n = 0
 
 def Main():
     x0, toler, iterMax = inputData()
     raiz = newtonRaphsonMethod(x0, toler, iterMax)
-    print(f"Raíz: {raiz}")
+    print(f"Raíz: {raiz:.4f}")
 
     
 if __name__ == "__main__":
